c4fb16a1c577dc94fb56ef943d99aa8db8eb60c3
eduardo <eduardo.74094@aluno.iffar.edu.br>
2026-05-03 14:31:34 -0300

    teste caminho index

--- back_end/server.js
+++ back_end/server.js
@@ -28,10 +28,18 @@ rotas(app);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("Servidor ligado!");
-});
 
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
 
