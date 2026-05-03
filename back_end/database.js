1cf414dd1bea60b2e4aae35a67abd46ef9bb1ab2
eduardo74094 <eduardo.74094@aluno.iffar.edu.br>
2026-05-03 12:59:13 -0300

    Update database.js

--- back_end/database.js
+++ back_end/database.js
@@ -1,22 +1,19 @@
-const {Pool} =require('pg');
-const Database={
-    conectar(){
-        globalThis.Database=new Pool({
-            host:"localhost",
-            user:"postgres",
-            port:5432,
-            max:20,
-            database:"Coelhos",
-            password:'alegrete',
-            idleTimeoutMillis: 30000, 
-            connectionTimeoutMillis: 2000,
-           
-        });
-    },
-    async query(sql,parametros=[]){
-        const result=await globalThis.Database.query(sql,parametros);
-      
-         return result.rows;
-    }
-}
-module.exports=Database;
+const { Pool } = require('pg');
+
+const Database = {
+  conectar() {
+    globalThis.Database = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    });
+  },
+
+  async query(sql, parametros = []) {
+    const result = await globalThis.Database.query(sql, parametros);
+    return result.rows;
+  }
+};
+
+module.exports = Database;
