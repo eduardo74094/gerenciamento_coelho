32fc0490b167b5ee1807c39087e9f9d5f782639b
eduardo <eduardo.74094@aluno.iffar.edu.br>
2026-05-24 16:13:29 -0300

    ultimas alteraçoes

--- back_end/model/CoelhoModel.js
+++ back_end/model/CoelhoModel.js
@@ -71,59 +71,60 @@ async updateCoelho(id, coelho) {
 
     const sql = `
     UPDATE coelho SET
-      numero_coelho=$1,
-      nome_coelho=$2,
-      raca_coelho=$3,
-      data_nascimento_coelho=$4,
-      sexo_coelho=$5,
-      observacoes_coelho=$6,
-      peso_nascimento=$7,
-      peso_atual=$8,
-      tipo_coelho=$9,
-      data_desmame=$10,
-      peso_desmame=$11,
-      matriz_coelho=$12,
-      reprodutor_coelho=$13,
-      situacao_coelho=$14,
-      transferido_coelho=$15,
-      foto_coelho=$16
-    WHERE id_coelho=$17
+      numero_coelho = $1,
+      nome_coelho = $2,
+      raca_coelho = $3,
+      data_nascimento_coelho = $4,
+      sexo_coelho = $5,
+      observacoes_coelho = $6,
+      peso_nascimento = $7,
+      peso_atual = $8,
+      tipo_coelho = $9,
+      data_desmame = $10,
+      peso_desmame = $11,
+      matriz_coelho = $12,
+      reprodutor_coelho = $13,
+      situacao_coelho = $14,
+      transferido_coelho = $15,
+      foto_coelho = $16
+    WHERE id_coelho = $17
     RETURNING *;
     `;
 
-    const res = await Database.query(sql, [
+    const valores = [
       coelho.numero_coelho,
       coelho.nome_coelho,
       coelho.raca_coelho,
       coelho.data_nascimento_coelho,
       coelho.sexo_coelho,
-      coelho.observacoes_coelho,
+      coelho.observacoes_coelho || null,
       coelho.peso_nascimento,
       coelho.peso_atual,
       coelho.tipo_coelho,
       coelho.data_desmame,
       coelho.peso_desmame,
-      coelho.matriz_coelho,
-      coelho.reprodutor_coelho,
+      coelho.matriz_coelho || null,
+      coelho.reprodutor_coelho || null,
+      coelho.situacao_coelho || null,
+      
+      // Transferido
+      (coelho.transferido_coelho === "1" || 
+       coelho.transferido_coelho === 1 || 
+       coelho.transferido_coelho === "true") ? 1 : null,
 
-      (
-        coelho.situacao_coelho !== undefined
-          ? coelho.situacao_coelho
-          : (coelho.situacao || null)
-      ),
-
-      coelho.transferido_coelho === "null" ||
-      coelho.transferido_coelho === "" ||
-      coelho.transferido_coelho === undefined
-        ? null
-        : Number(coelho.transferido_coelho),
-
-      coelho.foto_coelho,
+      // Foto - Muito importante!
+      coelho.foto_coelho || null,
 
       id
-    ]);
+    ];
 
-    return res && res[0] ? res[0] : null;
+    try {
+        const res = await Database.query(sql, valores);
+        return res && res[0] ? res[0] : null;
+    } catch (error) {
+        console.error("Erro ao atualizar coelho:", error);
+        throw error;
+    }
 }
 }
 module.exports = {
