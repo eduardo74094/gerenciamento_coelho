4dd2272fde5708d95c61d4683744d6fbccda6401
eduardo <eduardo.74094@aluno.iffar.edu.br>
2026-05-24 14:10:35 -0300

    ultimas alteraçoes

--- back_end/model/CoelhoModel.js
+++ back_end/model/CoelhoModel.js
@@ -66,12 +66,32 @@ async excluirCoelho(id){
 
    
 }
+
 async updateCoelho(id, coelho) {
-    const sql = (
-"UPDATE coelho SET nome_coelho=$1, raca_coelho=$2, data_nascimento_coelho=$3, sexo_coelho=$4, observacoes_coelho=$5, peso_nascimento=$6, peso_atual=$7, tipo_coelho=$8, data_desmame=$9, peso_desmame=$10, matriz_coelho=$11, reprodutor_coelho=$12, situacao_coelho=$13, transferido_coelho=$14 WHERE id_coelho=$15 RETURNING *"
-    );
+
+    const sql = `
+    UPDATE coelho SET
+      numero_coelho=$1,
+      nome_coelho=$2,
+      raca_coelho=$3,
+      data_nascimento_coelho=$4,
+      sexo_coelho=$5,
+      observacoes_coelho=$6,
+      peso_nascimento=$7,
+      peso_atual=$8,
+      tipo_coelho=$9,
+      data_desmame=$10,
+      peso_desmame=$11,
+      matriz_coelho=$12,
+      reprodutor_coelho=$13,
+      situacao_coelho=$14,
+      transferido_coelho=$15
+    WHERE id_coelho=$16
+    RETURNING *;
+    `;
 
     const res = await Database.query(sql, [
+      coelho.numero_coelho,
       coelho.nome_coelho,
       coelho.raca_coelho,
       coelho.data_nascimento_coelho,
@@ -82,13 +102,18 @@ async updateCoelho(id, coelho) {
       coelho.tipo_coelho,
       coelho.data_desmame,
       coelho.peso_desmame,
-  coelho.matriz_coelho,
-  coelho.reprodutor_coelho,
-  (coelho.situacao_coelho !== undefined ? coelho.situacao_coelho : (coelho.situacao || null)),
-  coelho.transferido_coelho || null,
+      coelho.matriz_coelho,
+      coelho.reprodutor_coelho,
+      (
+        coelho.situacao_coelho !== undefined
+          ? coelho.situacao_coelho
+          : (coelho.situacao || null)
+      ),
+      coelho.transferido_coelho || null,
       id
     ]);
-    return res && res[0] ? res[0] : null;
+
+    return res && res[0] ? res[0] : null;;
   }
 }
 
